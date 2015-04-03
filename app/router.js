c251fff9b7da2f68e0c37962a65be96ee30f7bc8
Joseph <jyo.lewis@gmail.com>
2015-04-02 22:09:53 -0400

    my blog using ember.js

--- app/router.js
+++ app/router.js
@@ -5,5 +5,12 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
+Router.map(function() {
+	this.route('about');
+	this.resource('posts', function() {
+		this.resource('post', { path: ':post_id' });
+	});
+});
+
 export default Router.map(function() {
 });
